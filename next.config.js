dead1ce1ec51e641a2f873826bfcfd85e8b7f01e
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2025-03-30 20:25:55 +0100

    msg2

--- next.config.js
+++ next.config.js
@@ -14,27 +14,27 @@ const nextConfig = {
     */
   },
   typescript: {
-    ignoreBuildErrors: true  // Add this line to temporarily bypass type errors
+    ignoreBuildErrors: true, // Add this line to temporarily bypass type errors
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'example.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "example.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        pathname: '/**',
-      }
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
+      },
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
-    formats: ['image/webp'],
+    formats: ["image/webp"],
   },
-  distDir: '.next'
-}
+  distDir: "build",
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
